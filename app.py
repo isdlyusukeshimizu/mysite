b1cae53c27022a5419f970a2f92fcb008b84d8f2
Yusuke Shimizu <159997300+isdlyusukeshimizu@users.noreply.github.com>
2024-06-09 01:37:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ app = Flask(__name__)
 def portfolio():
     return render_template('portfolio.html')
 
+#取り組み内容を時系列で表示するページ
 @app.route('/torikumi')
 def torikumi():
     return render_template('torikumi.html')
 
+#Docker勉強会の内容を表示するページ
 @app.route('/docker_lec')
 def docker_lec():
     return render_template('docker_lec.html')
-
+    
 @app.route('/media/<path:filename>')
 def media_files(filename):
     return send_from_directory('media', filename)
