446666ee063b0ab7a813cf4d331e9b718d556fb7
Yusuke Shimizu <159997300+isdlyusukeshimizu@users.noreply.github.com>
2024-06-28 13:57:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ def docker_lec():
 #画像ファイルをmediaフォルダから取得
 @app.route('/media/<path:filename>')
 def media_files(filename):
+    file_path = safe_join('media', filename)
+    if not os.path.isfile(file_path):
+        abort(404)  # ファイルが存在しない場合は404エラーを返す
     return send_from_directory('media', filename)
 
 if __name__ == '__main__':
